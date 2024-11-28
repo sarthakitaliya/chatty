ff54b6710014a84b372aac7bfdc0e60fda8c7c16
sarthak italiya <sarthak.italiya2005@gmail.com>
2024-11-28 11:48:30 +0530

    "Update CORS origin "

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json({limit: '50mb'}));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL],
+    origin: process.env.FRONTEND_URL,
     credentials: true,
   })
 );
