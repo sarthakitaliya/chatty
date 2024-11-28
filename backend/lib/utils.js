5b1a9c658d6b931658c9ec1a6f99006006cf2bc9
sarthak italiya <sarthak.italiya2005@gmail.com>
2024-11-28 11:56:57 +0530

    "Update cookie settings in generateToken function"

--- backend/lib/utils.js
+++ backend/lib/utils.js
@@ -7,8 +7,8 @@ export const generateToken = async (userId, res) => {
   res.cookie("token", token, {
     httpOnly: true,
     maxAge: 7 * 24 * 60 * 60 * 1000,
-    sameSite: "strict",
-    secure: process.env.NODE_ENV !== "development",
+    sameSite: "none",
+    secure: process.env.NODE_ENV == "production",
   });
   return token;
 };
